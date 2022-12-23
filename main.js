a92d627c2bf74caf8e537be723c22dbb09b03077
VoDA_Code <dimavovk201112@gmail.com>
2022-12-23 20:21:12 +0200

    Added debug mode

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ var data = {
     n: "0",
     d: 2,
     inputD: 10,
-    count: 8
+    count: 8,
+    debug: false,
+    info: "",
+    rs: 8
 };
 //load the data
 (function () {
@@ -13,11 +16,14 @@ var data = {
         "-n": "0",
         "-d": "2",
         "-input-d": "10",
-        "-c": '8'
+        "-c": '8',
+        "-debug": '0',
+        "-info": '',
+        "-rs": '8'
     };
     for (var i = 0; i < process.argv.length; i++) {
         for (var key in _data) {
-            if ((key == "-n" || key == "-d" || key == "-input-d" || key == "-c") && key == process.argv[i]) {
+            if ((key == "-n" || key == "-d" || key == "-input-d" || key == "-c" || key == "-debug" || key == "-info" || key == '-rs') && key == process.argv[i]) {
                 if (!process.argv[i + 1]) {
                     throw new Error("Invalid value");
                 }
@@ -29,7 +35,34 @@ var data = {
     data.d = Number(_data["-d"]);
     data.inputD = Number(_data["-input-d"]);
     data.n = _data["-n"];
+    data.info = _data["-info"];
+    data.rs = Number(_data["-rs"]);
+    data.debug = _data["-debug"] == '1' || _data["-debug"] == 'true';
 })();
+if (data.info.length > 0) {
+    var num = Number(data.info);
+    num = num | 0;
+    var res = "";
+    var d2 = translateNumber(Math.abs(num).toString(), 2, 10);
+    while (res.length + d2.length < data.rs) {
+        res += "0";
+    }
+    res += d2;
+    console.log("Direct code: ".concat(num >= 0 ? 0 : 1, ".").concat(res));
+    if (num >= 0) {
+        console.log("Additional code: 0.".concat(res));
+        console.log("Return code: 0.".concat(res));
+    }
+    else {
+        var tmp = "";
+        for (var i = 0; i < res.length; i++) {
+            tmp += res[i] == '0' ? '1' : '0';
+        }
+        console.log("Additional code: 1.".concat(plassOneLaftBit(tmp)));
+        console.log("Return code: 1.".concat(tmp));
+    }
+    process.exit(0);
+}
 var a = "";
 var b = "";
 (function () {
@@ -43,9 +76,16 @@ console.log("RESULT: ".concat(a, ".").concat(b));
 function translateFractional(n, to, from) {
     if (from === void 0) { from = 10; }
     var dNumber = new bignumber_js_1["default"](0);
+    var debug = "[TO 10 (F)]\n";
     if (from != 10) {
         for (var i = 0; i < n.length; i++) {
+            if (data.debug) {
+                debug += "".concat(!!Number(n[i]) || n[i] == '0' ? Number(n[i]) : n.charCodeAt(i) - 55, " * (").concat(from, "^-").concat(i + 1, ") + ");
+            }
             dNumber = dNumber.plus(new bignumber_js_1["default"]((!!Number(n[i]) || n[i] == '0' ? Number(n[i]) : n.charCodeAt(i) - 55) * (Math.pow(from, -(i + 1)))));
+        }
+        if (data.debug) {
+            debug += "= ".concat(dNumber, "\n\n");
         }
     }
     else {
@@ -54,20 +94,34 @@ function translateFractional(n, to, from) {
     var result = [];
     for (var i = 0; i < data.count; i++) {
         var tmp = dNumber.multipliedBy(to);
-        result.push(translateNumber(tmp.decimalPlaces(0, 1).toString(), to, 10));
-        dNumber = tmp.minus(tmp.decimalPlaces(0, 1));
+        var decimal = tmp.decimalPlaces(0, 1);
+        if (data.debug) {
+            console.log("".concat(dNumber.toString(), " * ").concat(to, " = ").concat(tmp.toString(), " (").concat(decimal.isLessThan(10) ? decimal.toString() : String.fromCharCode(decimal.plus(55).toNumber()).toUpperCase(), ")"));
+        }
+        result.push(translateNumber(decimal.toString(), to, 10));
+        dNumber = tmp.minus(decimal);
         if (dNumber.isEqualTo(0))
             break;
+    }
+    if (data.debug) {
+        console.log(debug);
     }
     return result.join('');
 }
 function translateNumber(n, to, from) {
     if (from === void 0) { from = 10; }
     var dNum = 0;
+    var debug = "[TO 10 (N)]\n";
     if (from != 10) {
         var tmp = 0;
         for (var i = 0; i < n.length; i++) {
+            if (data.debug) {
+                debug += "".concat(!!Number(n[i]) || n[i] == '0' ? Number(n[i]) : n.charCodeAt(i) - 55, " * (").concat(from, "^").concat(n.length - i - 1, ") + ");
+            }
             tmp += (!!Number(n[i]) || n[i] == '0' ? Number(n[i]) : n.charCodeAt(i) - 55) * (Math.pow(from, (n.length - i - 1)));
+        }
+        if (data.debug) {
+            debug += "= ".concat(tmp, "\n\n");
         }
         dNum = tmp;
     }
@@ -80,9 +134,34 @@ function translateNumber(n, to, from) {
         while (mod >= to) {
             mod -= to;
         }
-        store.push(mod < 10 ? mod.toString() : String.fromCharCode(mod + 55));
+        var c = mod < 10 ? mod.toString() : String.fromCharCode(mod + 55);
+        store.push(c);
+        var tmp = dNum;
         dNum = dNum / to | 0;
+        if (data.debug) {
+            debug += "".concat(tmp, " / ").concat(to, " = ").concat(dNum, " (").concat(dNum * to, ") [").concat(c, "]\n");
+        }
     }
     store.push(dNum < 10 ? dNum.toString() : String.fromCharCode(dNum + 55));
+    if (data.debug) {
+        debug += "[".concat(dNum, "]\n");
+        console.log(debug);
+    }
     return store.reverse().join('');
+}
+function plassOneLaftBit(n) {
+    var arr = n.split('');
+    var k = 1;
+    for (var i = arr.length - 1; i >= 0; i--) {
+        var num = Number(arr[i]) + k;
+        if (num > 1) {
+            arr[i] = '0';
+            k = 1;
+        }
+        else {
+            arr[i] = num.toString();
+            k = 0;
+        }
+    }
+    return arr.join('');
 }
--- main.ts
+++ main.ts
@@ -4,7 +4,10 @@ let data = {
     n: "0",
     d: 2,
     inputD: 10,
-    count: 8
+    count: 8,
+    debug: false,
+    info: "",
+    rs: 8
 };
 //load the data
 (() => {
@@ -12,12 +15,15 @@ let data = {
         "-n": "0",
         "-d": "2",
         "-input-d": "10",
-        "-c": '8'
+        "-c": '8',
+        "-debug": '0',
+        "-info": '',
+        "-rs": '8'
     };
 
     for (let i = 0; i < process.argv.length; i++) {
         for (const key in _data) {
-            if ((key == "-n" || key == "-d" || key == "-input-d" || key == "-c") && key == process.argv[i]) {
+            if ((key == "-n" || key == "-d" || key == "-input-d" || key == "-c" || key == "-debug" || key == "-info" || key == '-rs') && key == process.argv[i]) {
                 if (!process.argv[i + 1]) {
                     throw new Error("Invalid value");
                 }
@@ -29,7 +35,34 @@ let data = {
     data.d = Number(_data["-d"]);
     data.inputD = Number(_data["-input-d"]);
     data.n = _data["-n"];
+    data.info = _data["-info"];
+    data.rs = Number(_data["-rs"]);
+    data.debug = _data["-debug"] == '1' || _data["-debug"] == 'true';
 })();
+
+if(data.info.length > 0){
+    let num = Number(data.info);
+    num = num | 0;
+    let res = "";
+    let d2 = translateNumber(Math.abs(num).toString(), 2, 10);
+    while(res.length + d2.length < data.rs){
+        res += "0";
+    }
+    res += d2;
+    console.log(`Direct code: ${num >= 0 ? 0 : 1}.${res}`);
+    if(num >= 0){
+        console.log(`Additional code: 0.${res}`);
+        console.log(`Return code: 0.${res}`);
+    }else{
+        let tmp = "";
+        for(let i = 0; i < res.length; i++){
+            tmp += res[i] == '0' ? '1' : '0';
+        }
+        console.log(`Additional code: 1.${plassOneLaftBit(tmp)}`);
+        console.log(`Return code: 1.${tmp}`);
+    }
+    process.exit(0);
+}
 
 let a = "";
 let b = "";
@@ -44,31 +77,52 @@ b = translateFractional(b, data.d, data.inputD);
 console.log(`RESULT: ${a}.${b}`);
 
 function translateFractional(n: string, to: number, from: number = 10): string {
-    let dNumber:BigNumber = new BigNumber(0);
+    let dNumber: BigNumber = new BigNumber(0);
+    let debug = "[TO 10 (F)]\n";
     if (from != 10) {
-        for(let i = 0; i < n.length; i++) {
+        for (let i = 0; i < n.length; i++) {
+            if(data.debug){
+                debug += `${!!Number(n[i]) || n[i] == '0' ? Number(n[i]) : n.charCodeAt(i) - 55} * (${from}^-${i + 1}) + `;
+            }
             dNumber = dNumber.plus(new BigNumber((!!Number(n[i]) || n[i] == '0' ? Number(n[i]) : n.charCodeAt(i) - 55) * (from ** -(i + 1))));
         }
+        if(data.debug){
+            debug += `= ${dNumber}\n\n`;
+        }
     } else {
-        dNumber = new BigNumber("0."+n);
+        dNumber = new BigNumber("0." + n);
     }
     let result: string[] = [];
     for (let i = 0; i < data.count; i++) {
         let tmp = dNumber.multipliedBy(to);
-        result.push(translateNumber(tmp.decimalPlaces(0, 1).toString(), to, 10));
-        dNumber = tmp.minus(tmp.decimalPlaces(0, 1));
+        let decimal = tmp.decimalPlaces(0, 1);
+        if (data.debug) {
+            console.log(`${dNumber.toString()} * ${to} = ${tmp.toString()} (${decimal.isLessThan(10) ? decimal.toString() : String.fromCharCode(decimal.plus(55).toNumber()).toUpperCase()})`)
+        }
+        result.push(translateNumber(decimal.toString(), to, 10));
+        dNumber = tmp.minus(decimal);
         if (dNumber.isEqualTo(0))
             break;
+    }
+    if(data.debug){
+        console.log(debug);
     }
     return result.join('');
 }
 
 function translateNumber(n: string, to: number, from: number = 10): string {
     let dNum = 0;
+    let debug = "[TO 10 (N)]\n";
     if (from != 10) {
         let tmp = 0;
         for (let i = 0; i < n.length; i++) {
+            if(data.debug){
+                debug += `${!!Number(n[i]) || n[i] == '0' ? Number(n[i]) : n.charCodeAt(i) - 55} * (${from}^${n.length - i - 1}) + `;
+            }
             tmp += (!!Number(n[i]) || n[i] == '0' ? Number(n[i]) : n.charCodeAt(i) - 55) * (from ** (n.length - i - 1));
+        }
+        if(data.debug){
+            debug += `= ${tmp}\n\n`;
         }
         dNum = tmp;
     }
@@ -77,13 +131,39 @@ function translateNumber(n: string, to: number, from: number = 10): string {
     }
     let store: string[] = [];
     while (dNum >= to) {
+        
         let mod = dNum % to;
         while (mod >= to) {
             mod -= to;
         }
-        store.push(mod < 10 ? mod.toString() : String.fromCharCode(mod + 55));
+        let c = mod < 10 ? mod.toString() : String.fromCharCode(mod + 55);
+        store.push(c);
+        let tmp = dNum;
         dNum = dNum / to | 0;
+        if(data.debug){
+            debug += `${tmp} / ${to} = ${dNum} (${dNum * to}) [${c}]\n`;
+        }
     }
     store.push(dNum < 10 ? dNum.toString() : String.fromCharCode(dNum + 55));
+    if(data.debug){
+        debug += `[${dNum}]\n`
+        console.log(debug);
+    }
     return store.reverse().join('');
+}
+
+function plassOneLaftBit(n: string): string {
+    let arr = n.split('');
+    let k = 1;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let num = Number(arr[i]) + k;
+        if(num > 1){
+            arr[i] = '0';
+            k = 1;
+        }else{
+            arr[i] = num.toString();
+            k = 0;
+        }
+    }
+    return arr.join('');
 }
